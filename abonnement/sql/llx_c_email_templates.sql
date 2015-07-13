4884ac7af3737051365e6283376e1cad71769b3a
ares <aresvouk@yahoo.fr>
2015-07-13 16:10:17 +0000

    ajout des templates des emails

---
+++ abonnement/sql/llx_c_email_templates.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO `llx_c_email_templates` (`rowid`, `entity`, `module`, `type_template`, `lang`, `private`, `fk_user`, `datec`, `tms`, `label`, `position`, `active`, `topic`, `content`) VALUES
+(1, 1, NULL, 'reabonnement', NULL, 0, NULL, NULL, '2015-07-10 19:51:55', 'réabonnement', 12, 1, 'Réabonnement de client', 'Cher client votre abonnementva expiré le __DATE_FIN_VALIDER,\r\nveuiilez trouver ci-joint la facture pour votre reabonnement'),
+(2, 1, NULL, 'user_abonne_create', NULL, 0, NULL, NULL, '2015-07-13 15:50:37', 'creation code accès', 1001, 1, 'Création code d''accès chez alter', '\r\nBonjour \r\nVous avez demandé un abonnement chez alter , votre demande a  été  bien reçue ,\r\nVoici  vos identifiants pour vous connecter :\r\n\r\nLogin : __LOGIN__\r\nMot de passe : __PASSWORD__\r\n\r\nCliquez ici pour aller sur : __URL__');
+
